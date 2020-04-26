e861552a84dde1700701fe0980f986cfbf02a3e9
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2020-04-26 14:08:39 +0800

    Initial implementation

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighboors_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighboor_row = (row + delta_row) % self.height;
+                let neighboor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighboor_row, neighboor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                println!("idx = {} {}", row, col);
+                let idx = self.get_index(row, col);
+                println!("idx dx = {}", idx);
+                println!("Cells {:#?}", self.cells.len());
+                let cell = self.cells[idx];
+                let live_neighboors = self.live_neighboors_count(row, col);
+                println!("HERE");
+
+                let next_cell = match (cell, live_neighboors) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn new(size: u32) -> Universe {
+        let width = size;
+        let height = size;
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 { Cell::Alive }
+                else { Cell::Dead }
+            }).collect();
+
+        Universe { width, height, cells }
+    }
+
+    pub fn render(&self) -> String { self.to_string() }
+}
+
+use std::fmt;
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }    
+}
+
+#[test]
+fn unreachable_executed() {
+    let mut universe = Universe::new(4);
+
+    for _ in 0..10 {
+        println!("{}", universe.to_string());
+        universe.tick();
+    }
+    println!("{}", universe.to_string());
+}
