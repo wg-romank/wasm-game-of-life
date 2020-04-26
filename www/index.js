ba9ab54500b84f1ca3ac2a08eeb890c0ded6ed8d
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2020-04-26 15:46:49 +0800

    Draw from memory

--- www/index.js
+++ www/index.js
@@ -1,13 +1,77 @@
-import { Universe } from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+const CELL_SIZE = 5;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
 
-const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new(64);
+const width = universe.width();
+const height = universe.height();
+
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.height = (CELL_SIZE + 1) * height + 1;
+
+const ctx = canvas.getContext("2d");
+
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeKey = GRID_COLOR;
+
+    for (let i = 0; i < width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height, 1);
+    }
+
+    for (let j = 0; j < height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+}
+
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+const getIndex = (row, col) => {
+    return row * width + col;
+}
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+}
 
 const renderLoop = () => {
-    pre.textContent = universe.render();
     universe.tick();
+
+    drawGrid();
+    drawCells();
 
     requestAnimationFrame(renderLoop);
 }
 
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
