906c912072d9228e7ffcc224c923c99a7f3beb8c
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2020-05-01 23:38:05 +0800

    Move shaders to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,24 @@
 use std::collections::HashSet;
 
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{WebGlProgram, WebGlRenderingContext as WebGl, WebGlShader};
+use web_sys::{WebGlRenderingContext as WebGl};
 
 mod universe;
+mod shaders;
 
 const CELL_SIZE: u32 = 5;
 
-fn get_canvas() -> Option<web_sys::HtmlCanvasElement> {
-    let document = web_sys::window()?.document()?;
-    let canvas = document.get_element_by_id("game-of-life-canvas")?;
-
-    canvas.dyn_into::<web_sys::HtmlCanvasElement>().ok()
-}
-
-fn get_ctx<T : JsCast>(ctx_name: &str) -> Result<T, JsValue> {
-    let ctx = get_canvas()
-        .ok_or(JsValue::from_str("Failed to get canvas"))?
-        .get_context(ctx_name)?
-        .ok_or(JsValue::from_str("Failed getting ctx"))?;
-
-    ctx.dyn_into::<T>()
-        .map_err(|e| JsValue::from(e))
-}
 
 #[wasm_bindgen]
 pub fn get_cell_size() -> Result<u32, JsValue> { Ok(CELL_SIZE) }
 
 #[wasm_bindgen]
 pub fn setup_canvas(universe: &universe::Universe) -> Result<(), JsValue> {
-    let canvas = get_canvas().ok_or(JsValue::from_str("Failed getting canvas"))?;
+    let canvas = shaders::get_canvas().ok_or(JsValue::from_str("Failed getting canvas"))?;
     canvas.set_width((CELL_SIZE + 1) * universe.width() + 1);
     canvas.set_height((CELL_SIZE + 1) * universe.height() + 1);
 
     Ok(())
-}
-
-fn setup_shaders() -> Result<WebGl, JsValue> {
-    let canvas = get_canvas().ok_or(JsValue::from_str("Failed to get canvas"))?;
-    let context: WebGl = get_ctx("webgl")?;
-
-    let vert_shader = compile_shader(
-        &context,
-        WebGl::VERTEX_SHADER,
-        r#"
-        precision highp float;
-
-        attribute vec2 position;
-
-        uniform vec2 canvasSize;
-
-        void main() {
-            vec2 zeroOne = position / canvasSize;
-            vec2 clipSpace = zeroOne * 2.0 - 1.0;
-            gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-        }
-    "#,
-    )?;
-    let frag_shader = compile_shader(
-        &context,
-        WebGl::FRAGMENT_SHADER,
-        r#"
-        precision highp float;
-        void main() {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-        }
-    "#,
-    )?;
-    let program = link_program(&context, &vert_shader, &frag_shader)?;
-    context.use_program(Some(&program));
-
-    let canvas_size = context.get_uniform_location(&program, "canvasSize")
-        .ok_or(JsValue::from(
-            format!("Failed to get uniform uCol: {}", context.get_error())
-        )
-        )?;
-
-    context.uniform2f(Some(&canvas_size), canvas.width() as f32, canvas.height() as f32);
-
-    Ok(context)
 }
 
 fn compute_draw_cells_webgl(changes: &HashSet<(u32, u32)>) -> Vec<f32> {
@@ -104,7 +43,7 @@ fn compute_draw_cells_webgl(changes: &HashSet<(u32, u32)>) -> Vec<f32> {
 
 #[wasm_bindgen]
 pub fn setup_webgl() -> Result<(), JsValue> {
-    setup_shaders()?;
+    shaders::setup_shaders()?;
 
     Ok(())
 }
@@ -113,7 +52,7 @@ pub fn setup_webgl() -> Result<(), JsValue> {
 pub fn animation_webgl(universe: &mut universe::Universe, ticks: u32) -> Result<(), JsValue> {
     universe.tick_many(ticks);
 
-    let context: WebGl = get_ctx("webgl")?;
+    let context: WebGl = shaders::get_ctx("webgl")?;
     let vertices = compute_draw_cells_webgl(universe.alive_cells());
 
     let buffer = context.create_buffer().ok_or("failed to create buffer")?;
@@ -140,54 +79,4 @@ pub fn animation_webgl(universe: &mut universe::Universe, ticks: u32) -> Result<
 
     }
     Ok(())
-}
-
-fn compile_shader(
-    context: &WebGl,
-    shader_type: u32,
-    source: &str,
-) -> Result<WebGlShader, String> {
-    let shader = context
-        .create_shader(shader_type)
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-    context.shader_source(&shader, source);
-    context.compile_shader(&shader);
-
-    if context
-        .get_shader_parameter(&shader, WebGl::COMPILE_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(shader)
-    } else {
-        Err(context
-            .get_shader_info_log(&shader)
-            .unwrap_or_else(|| String::from("Unknown error creating shader")))
-    }
-}
-
-fn link_program(
-    context: &WebGl,
-    vert_shader: &WebGlShader,
-    frag_shader: &WebGlShader,
-) -> Result<WebGlProgram, String> {
-    let program = context
-        .create_program()
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-
-    context.attach_shader(&program, vert_shader);
-    context.attach_shader(&program, frag_shader);
-    context.link_program(&program);
-
-    if context
-        .get_program_parameter(&program, WebGl::LINK_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(program)
-    } else {
-        Err(context
-            .get_program_info_log(&program)
-            .unwrap_or_else(|| String::from("Unknown error creating program object")))
-    }
 }
---
+++ src/shaders.rs
@@ -0,0 +1,116 @@
+use web_sys::{WebGlProgram, WebGlRenderingContext as WebGl, WebGlShader};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+
+fn compile_shader(
+    context: &WebGl,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGl::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+fn link_program(
+    context: &WebGl,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGl::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+    }
+}
+
+pub fn get_canvas() -> Option<web_sys::HtmlCanvasElement> {
+    let document = web_sys::window()?.document()?;
+    let canvas = document.get_element_by_id("game-of-life-canvas")?;
+
+    canvas.dyn_into::<web_sys::HtmlCanvasElement>().ok()
+}
+
+pub fn get_ctx<T : JsCast>(ctx_name: &str) -> Result<T, JsValue> {
+    let ctx = get_canvas()
+        .ok_or(JsValue::from_str("Failed to get canvas"))?
+        .get_context(ctx_name)?
+        .ok_or(JsValue::from_str("Failed getting ctx"))?;
+
+    ctx.dyn_into::<T>()
+        .map_err(|e| JsValue::from(e))
+}
+
+pub fn setup_shaders() -> Result<WebGl, JsValue> {
+    let canvas = get_canvas().ok_or(JsValue::from_str("Failed to get canvas"))?;
+    let context: WebGl = get_ctx("webgl")?;
+
+    let vert_shader = compile_shader(
+        &context,
+        WebGl::VERTEX_SHADER,
+        r#"
+        precision highp float;
+
+        attribute vec2 position;
+
+        uniform vec2 canvasSize;
+
+        void main() {
+            vec2 zeroOne = position / canvasSize;
+            vec2 clipSpace = zeroOne * 2.0 - 1.0;
+            gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+        }
+    "#,
+    )?;
+    let frag_shader = compile_shader(
+        &context,
+        WebGl::FRAGMENT_SHADER,
+        r#"
+        precision highp float;
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+    "#,
+    )?;
+    let program = link_program(&context, &vert_shader, &frag_shader)?;
+    context.use_program(Some(&program));
+
+    let canvas_size = context.get_uniform_location(&program, "canvasSize")
+        .ok_or(JsValue::from(
+            format!("Failed to get uniform uCol: {}", context.get_error())
+        )
+        )?;
+
+    context.uniform2f(Some(&canvas_size), canvas.width() as f32, canvas.height() as f32);
+
+    Ok(context)
+}
