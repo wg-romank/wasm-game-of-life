132b001fa8071c0316f643545ac4e24db1838173
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2020-05-30 16:37:54 +0800

    Use scaling for different devices

--- www/index.js
+++ www/index.js
@@ -1,7 +1,6 @@
 import { setup_copy_program, setup_init_program, setup_compute_program, animation_webgl, setup_webgl } from "wasm-game-of-life";
 
-let lastCall = 0;
-let cum = 0;
+let ppi = window.devicePixelRatio * 96;
 
 let canvas = document.getElementById("game-of-life-canvas");
 let brect = canvas.getBoundingClientRect();
@@ -9,11 +8,17 @@ let brect = canvas.getBoundingClientRect();
 let oWidth = brect.width;
 let oHeight = brect.height;
 
-let mWidth = Math.floor(brect.width / 4);
-let mHeight = Math.floor(brect.height / 4)
-
 canvas.setAttribute('width', oWidth);
 canvas.setAttribute('height', oHeight);
+
+let cellsPerInch = 25;
+
+let mWidth = Math.floor(brect.width / ppi * cellsPerInch);
+let mHeight = Math.floor(brect.height / ppi * cellsPerInch)
+
+console.log(ppi)
+console.log(mWidth);
+console.log(mHeight);
 
 let state = setup_webgl(mWidth, mHeight);
 let init_program = setup_init_program();
@@ -25,6 +30,8 @@ for (let i = 0; i < 100; i+= 1) {
   animation_webgl(init_program, compute_program, copy_program, mWidth, mHeight, state);
 }
 
+let lastCall = 0;
+let cum = 0;
 
 const renderLoop = (timestamp) => {
   const delta = timestamp - lastCall;
